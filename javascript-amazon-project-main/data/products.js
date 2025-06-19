96d699ab9978929fc14c87de3deb3cf3b62bc67b
shalaka149 <shalakakelkar25@gmail.com>
2025-06-19 13:14:01 +0530

    used inheritance to further organise code

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
 export function getProduct(productId) {
   let matchingProduct;
   
@@ -24,24 +26,36 @@ class Product{
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
   }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+      return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return '';
+  }
+
 }
 
-const product1 = new Product({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  });
-console.log(product1);
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target ="_blank">Size Chart</a>    
+    `;
+  }
+
+}
 
 export const products = [
   {
@@ -147,7 +161,9 @@ export const products = [
       "hoodies",
       "sweaters",
       "apparel"
-    ]
+    ],
+     type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   },
   {
     id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
@@ -700,6 +716,13 @@ export const products = [
       "hoodies",
       "apparel",
       "mens"
-    ]
+    ],
+     type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   }
-];
+].map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+});
